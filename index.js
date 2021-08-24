df81fbd7e95ae7fbad91e1c583c1ea4a472d7d3f
Accountant-riyad <Accountant-riyad>
2021-08-24 23:38:32 +0600

    done

--- index.js
+++ index.js
@@ -26,90 +26,45 @@ const client = new MongoClient(uri, {
 client.connect((err) => {
 
   const jobseekersCollection = client.db("virtual-jobseekers").collection("jobseekers");
-  const reviewCollection = client.db("virtual-jobseekers").collection("review");
-  const adminCollection = client.db("virtual-jobseekers").collection("admin");
+ 
   app.get("/", (req, res) => {
     res.send("hello from db it's working working");
   
   });
-  app.get("/service", (req, res) => {
+  app.get("/jobseekers", (req, res) => {
     jobseekersCollection.find({}).toArray((err, documents) => {
       res.send(documents);
     });
   });
 
-  app.get("/orders", (req, res) => {
-    ordersCollection.find({}).toArray((err, documents) => {
+  app.get("/searchedJob", (req, res) => {
+    const search= req.query.search
+    jobseekersCollection.find({location:{$regex:search}}).toArray((err, documents) => {
       res.send(documents);
     });
   });
-  app.get("/reviews", (req, res) => {
-    reviewCollection.find({}).toArray((err, documents) => {
-      res.send(documents);
-    });
-  });
-    app.get("/admin", (req, res) => {
-      adminCollection.find({}).toArray((err, documents) => {
-        res.send(documents);
-      });
-    });
+  
 
     app.post("/addEmployee", (req, res) => {
-      // const file = req.files.file;
-      console.log(req.body.FirstName);
-      // const name = req.body.name;
-      // const description = req.body.description;
-      // const price = req.body.price;
-      // const newImg = file.data;
-      // const encImg = newImg.toString("base64");
-  
-      // var image = {
-      //   contentType: file.mimetype,
-      //   size: file.size,
-      //   img: Buffer.from(encImg, "base64"),
-      // };
-      // console.log(name, description, image, price);
-      // serviceCollection
-      //   .insertOne({ name, description, image, price })
-      //   .then((result) => {
-      //     res.send(result.insertedCount > 0);
-      //   });
+    
+      console.log(req.body);
+       jobseekersCollection
+       .insertOne(req.body)
+        .then((result) => {
+          res.send(result.insertedCount > 0);
+        });
     });
   
   
 
+    
 
 
 
-  // app.post("/addEmployee", (req, res) => {
-  //   const orders = req.body;
-  //   ordersCollection.insertOne(orders).then((result) => {
-  //     res.send(result.insertedCount > 0);
-  //   });
-  // });
-
-  app.post("/addAdmin", (req, res) => {
-    const admin = req.body;
-    console.log(admin);
-    adminCollection.insertOne(admin).then((result) => {
-      res.send(result.insertedCount > 0);
-    });
-  });
-
-  app.post("/isAdmin", (req, res) => {
-    const email = req.body.email;
   
-    adminCollection.find({ email: email }).toArray((err, admin) => {
-      res.send(admin.length > 0);
-    });
-  });
+ 
+  
 
-  app.post("/addReview", (req, res) => {
-    const review = req.body;
-    reviewCollection.insertOne(review).then((result) => {
-      res.send(result.insertedCount > 0);
-    });
-  });
 
 
 
